d3d1fc7364b477a0040a35716a8d9b0057954593
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-04-22 10:34:24 +0200

    add sql data

---
+++ java/leetcode/top interview questions/easy/spring-jdbc-template/src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+insert into books(title, author) values ('Java', 'Bloch');
+insert into books(title, author) values ('jdbc', 'Freeman');
+insert into books(title, author) values ('Spring', 'Bloch');
