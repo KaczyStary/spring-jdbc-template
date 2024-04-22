be2ba5827687d50fdce5e6b40ffe4227eba57073
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-04-22 12:07:14 +0200

    move

--- java/leetcode/top interview questions/easy/spring-jdbc-template/src/main/resources/sql/data.sql
+++
@@ -1,3 +0,0 @@
-insert into books(title, author) values ('Java', 'Bloch');
-insert into books(title, author) values ('jdbc', 'Freeman');
-insert into books(title, author) values ('Spring', 'Bloch');
--- java/leetcode/top interview questions/easy/spring-jdbc-template/src/main/resources/sql/schema.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists books(
-    id integer identity primary key,
-    title varchar(50),
-    author varchar(50)
-);
